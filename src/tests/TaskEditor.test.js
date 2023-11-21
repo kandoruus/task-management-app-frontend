75e8f8794ea367f8b9ae164d5fd0d1195c537dbe
caleb <1d0nt3v3nl1ftbr0@gmail.com>
2023-11-21 13:07:40 -0500

    Completed test for if TaskEditor renders all expect elements

--- src/tests/TaskEditor.test.js
+++ src/tests/TaskEditor.test.js
@@ -22,29 +22,25 @@ function setup(jsx) {
 
 describe('TaskEditor', () => {
   it('has a name field, a description field, a status field, a priority field, a cancel button, a delete button, a save button, and a save and exit button.', () => {
-    renderWithProviders(<TaskEditor />, {
-      preloadedState: {
-        taskCtrl: initialTaskCtrlState,
-        taskEditor: {
-          ...initialTaskEditorState,
-          indexOfFocus: 0,
-        },
-      },
-    });
-    expect(
-      screen.getByText(initialTaskEditorState.data.name)
-    ).toBeInTheDocument();
-    expect(
-      screen.getByText(initialTaskEditorState.data.description)
-    ).toBeInTheDocument();
-    const selectBoxes = screen.getAllByRole('combobox');
-    expect(selectBoxes.length).toEqual(2);
-    expect(selectBoxes.indexOf(initialTaskEditorState.data.status)).not.toEqual(
-      -1
-    );
-    expect(
-      selectBoxes.indexOf(initialTaskEditorState.data.priority)
-    ).not.toEqual(-1);
+    renderWithProviders(<TaskEditor />);
+
+    //has a name and a description field
+    const textboxes = screen
+      .getAllByRole('textbox')
+      .map((textbox) => textbox.name);
+    expect(textboxes.length).toEqual(2);
+    expect(textboxes.indexOf('name')).not.toEqual(-1);
+    expect(textboxes.indexOf('description')).not.toEqual(-1);
+
+    //has a status field and a priority field
+    const comboboxes = screen
+      .getAllByRole('combobox')
+      .map((combobox) => combobox.name);
+    expect(comboboxes.length).toEqual(2);
+    expect(comboboxes.indexOf('status')).not.toEqual(-1);
+    expect(comboboxes.indexOf('priority')).not.toEqual(-1);
+
+    // has a cancel button, a delete button, a save button, and a save and exit button.
     expect(screen.getByText('Cancel')).toBeInTheDocument();
     expect(screen.getByText('Delete')).toBeInTheDocument();
     expect(screen.getByText('Save')).toBeInTheDocument();
@@ -59,43 +55,43 @@ describe('TaskEditor', () => {
           tasklist: getMockTasklist(mockTasklistLength),
         },
         taskEditor: {
-          ...getMockTasklist(mockTasklistLength).data,
+          ...getMockTasklist(mockTasklistLength)[0].data,
           indexOfFocus: 0,
         },
       },
     });
     expect(true).toBeFalsy();
-  });
+  }); /*
   it('the cancel button changes the flag showEditor to false, and changed data is not saved to the tasklist.', () => {
     renderWithProviders(<TaskEditor />);
     expect(true).toBeFalsy();
-  });
+  }); /*
   it('the delete button changes the showEditor flag to false and removes the task from the tasklist and the database', () => {
     renderWithProviders(<TaskEditor />);
     expect(true).toBeFalsy();
-  });
+  }); /*
   it("the save button updates the tasks's data in the tasklist and does not change the showEditor flag", () => {
     renderWithProviders(<TaskEditor />);
     expect(true).toBeFalsy();
-  });
+  }); /*
   it("the save and exit button updates the task's data in the tasklist and changes the showEditor flag to false", () => {
     renderWithProviders(<TaskEditor />);
     expect(true).toBeFalsy();
-  });
+  }); /*
   it('on change to the name input field, the taskEditor.data.name will update to match', () => {
     renderWithProviders(<TaskEditor />);
     expect(true).toBeFalsy();
-  });
+  }); /*
   it('on change to the description field, the taskEditor.data.description will update to match', () => {
     renderWithProviders(<TaskEditor />);
     expect(true).toBeFalsy();
-  });
+  }); /*
   it('on change to the status field the taskEditor.data.status will update to match', () => {
     renderWithProviders(<TaskEditor />);
     expect(true).toBeFalsy();
-  });
+  }); /*
   it('on change to the priority field the taskEditor.data.priority will update to match', () => {
     renderWithProviders(<TaskEditor />);
-    expect(true).toBeFalsy()w;
-  });*/
+    expect(true).toBeFalsy();
+  }); */
 });
