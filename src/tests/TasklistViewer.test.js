83afeac45fb6e26aec73e490752bb158716e8d77
caleb <1d0nt3v3nl1ftbr0@gmail.com>
2023-11-22 12:11:26 -0500

    Updated the nav button tests in the TasklistViewer test suite so that there is an indivual test for each nav button

--- src/tests/TasklistViewer.test.js
+++ src/tests/TasklistViewer.test.js
@@ -11,7 +11,6 @@ import { TASKS_PER_PAGE } from 'helper/constants';
 describe('TasklistViewer', () => {
   it('renders TasklistViewer with no TaskCards', () => {
     renderWithProviders(<TasklistViewer />);
-
     expect(screen.getByTestId('tasklist-viewer')).toBeInTheDocument();
     expect(screen.getAllByRole('button').length).toEqual(4);
     expect(screen.getByText('First')).toBeInTheDocument();
@@ -53,65 +52,81 @@ describe('TasklistViewer', () => {
     expect(screen.queryByText('mock name ' + TASKS_PER_PAGE)).toBeNull();
     expect(screen.getByText('1 of 2')).toBeInTheDocument();
   });
-  it('navigates between pages when the nav buttons are clicked', () => {
-    const mockTasklistLength = 101;
-    renderWithProviders(<TasklistViewer />, {
-      preloadedState: {
-        taskCtrl: {
-          ...initialTaskCtrlState,
-          tasklist: getMockTasklist(mockTasklistLength),
+  describe('with a large tasklist', () => {
+    beforeEach(() => {
+      const mockTasklistLength = 101;
+      renderWithProviders(<TasklistViewer />, {
+        preloadedState: {
+          taskCtrl: {
+            ...initialTaskCtrlState,
+            tasklist: getMockTasklist(mockTasklistLength),
+            page: 3,
+          },
+          taskEditor: initialTaskEditorState,
         },
-        taskEditor: initialTaskEditorState,
-      },
+      });
     });
-    for (let i = 0; i < TASKS_PER_PAGE; i++) {
-      expect(screen.getByText('mock name ' + i)).toBeInTheDocument();
-    }
-    expect(screen.queryByText('mock name ' + TASKS_PER_PAGE)).toBeNull();
-    expect(screen.getByText('1 of 5')).toBeInTheDocument();
-    //test next page
-    fireEvent.click(screen.getByText('>'));
-    expect(screen.queryByText('mock name ' + TASKS_PER_PAGE - 1)).toBeNull();
-    for (let i = TASKS_PER_PAGE; i < 2 * TASKS_PER_PAGE; i++) {
-      expect(screen.getByText('mock name ' + i)).toBeInTheDocument();
-    }
-    expect(screen.queryByText('mock name ' + 2 * TASKS_PER_PAGE)).toBeNull();
-    expect(screen.getByText('2 of 5')).toBeInTheDocument();
-    //test last page
-    fireEvent.click(screen.getByText('Last'));
-    expect(screen.queryByText('mock name 99')).toBeNull();
-    expect(screen.getByText('mock name 100')).toBeInTheDocument();
-    expect(screen.queryByText('mock name 101')).toBeNull();
-    expect(screen.getByText('5 of 5')).toBeInTheDocument();
-    //test next doesn't overflow
-    fireEvent.click(screen.getByText('Last'));
-    expect(screen.queryByText('mock name 99')).toBeNull();
-    expect(screen.getByText('mock name 100')).toBeInTheDocument();
-    expect(screen.queryByText('mock name 101')).toBeNull();
-    expect(screen.getByText('5 of 5')).toBeInTheDocument();
-    //test prev page
-    fireEvent.click(screen.getByText('<'));
-    expect(
-      screen.queryByText('mock name ' + 3 * TASKS_PER_PAGE - 1)
-    ).toBeNull();
-    for (let i = 3 * TASKS_PER_PAGE; i < 4 * TASKS_PER_PAGE; i++) {
-      expect(screen.getByText('mock name ' + i)).toBeInTheDocument();
-    }
-    expect(screen.queryByText('mock name ' + 4 * TASKS_PER_PAGE)).toBeNull();
-    expect(screen.getByText('4 of 5')).toBeInTheDocument();
-    //test first page
-    fireEvent.click(screen.getByText('First'));
-    for (let i = 0; i < TASKS_PER_PAGE; i++) {
-      expect(screen.getByText('mock name ' + i)).toBeInTheDocument();
-    }
-    expect(screen.queryByText('mock name ' + TASKS_PER_PAGE)).toBeNull();
-    expect(screen.getByText('1 of 5')).toBeInTheDocument();
-    //test prev doesn't overflow
-    fireEvent.click(screen.getByText('<'));
-    for (let i = 0; i < TASKS_PER_PAGE; i++) {
-      expect(screen.getByText('mock name ' + i)).toBeInTheDocument();
-    }
-    expect(screen.queryByText('mock name ' + TASKS_PER_PAGE)).toBeNull();
-    expect(screen.getByText('1 of 5')).toBeInTheDocument();
+    it('displays exactly 25 TaskCards, the correct current page, and the correct number of pages', () => {
+      expect(
+        screen.queryByText('mock name ' + 2 * TASKS_PER_PAGE - 1)
+      ).toBeNull();
+      for (let i = 2 * TASKS_PER_PAGE; i < 3 * TASKS_PER_PAGE; i++) {
+        expect(screen.getByText('mock name ' + i)).toBeInTheDocument();
+      }
+      expect(screen.queryByText('mock name ' + 3 * TASKS_PER_PAGE)).toBeNull();
+      expect(screen.getByText('3 of 5')).toBeInTheDocument();
+    });
+    it('navigates to the next page when the > button is clicked', () => {
+      fireEvent.click(screen.getByText('>'));
+      expect(
+        screen.queryByText('mock name ' + 3 * TASKS_PER_PAGE - 1)
+      ).toBeNull();
+      for (let i = 3 * TASKS_PER_PAGE; i < 4 * TASKS_PER_PAGE; i++) {
+        expect(screen.getByText('mock name ' + i)).toBeInTheDocument();
+      }
+      expect(screen.queryByText('mock name ' + 2 * TASKS_PER_PAGE)).toBeNull();
+      expect(screen.getByText('4 of 5')).toBeInTheDocument();
+    });
+    it('navigates to the previous page when the < button is clicked', () => {
+      fireEvent.click(screen.getByText('<'));
+      expect(screen.queryByText('mock name ' + TASKS_PER_PAGE - 1)).toBeNull();
+      for (let i = TASKS_PER_PAGE; i < 2 * TASKS_PER_PAGE; i++) {
+        expect(screen.getByText('mock name ' + i)).toBeInTheDocument();
+      }
+      expect(screen.queryByText('mock name ' + 2 * TASKS_PER_PAGE)).toBeNull();
+      expect(screen.getByText('2 of 5')).toBeInTheDocument();
+    });
+    it('navigates to the last page when the Last button is clicked', () => {
+      fireEvent.click(screen.getByText('Last'));
+      expect(screen.queryByText('mock name 99')).toBeNull();
+      expect(screen.getByText('mock name 100')).toBeInTheDocument();
+      expect(screen.queryByText('mock name 101')).toBeNull();
+      expect(screen.getByText('5 of 5')).toBeInTheDocument();
+    });
+    it('navigates to the first page when the First button is clicked', () => {
+      fireEvent.click(screen.getByText('First'));
+      for (let i = 0; i < TASKS_PER_PAGE; i++) {
+        expect(screen.getByText('mock name ' + i)).toBeInTheDocument();
+      }
+      expect(screen.queryByText('mock name ' + TASKS_PER_PAGE)).toBeNull();
+      expect(screen.getByText('1 of 5')).toBeInTheDocument();
+    });
+    it('does not change the page when the < is clicked and the current page is the first page', () => {
+      fireEvent.click(screen.getByText('First'));
+      fireEvent.click(screen.getByText('<'));
+      for (let i = 0; i < TASKS_PER_PAGE; i++) {
+        expect(screen.getByText('mock name ' + i)).toBeInTheDocument();
+      }
+      expect(screen.queryByText('mock name ' + TASKS_PER_PAGE)).toBeNull();
+      expect(screen.getByText('1 of 5')).toBeInTheDocument();
+    });
+    it('does not change the page when the > is clicked and the current page is the last page', () => {
+      fireEvent.click(screen.getByText('Last'));
+      fireEvent.click(screen.getByText('>'));
+      expect(screen.queryByText('mock name 99')).toBeNull();
+      expect(screen.getByText('mock name 100')).toBeInTheDocument();
+      expect(screen.queryByText('mock name 101')).toBeNull();
+      expect(screen.getByText('5 of 5')).toBeInTheDocument();
+    });
   });
 });
