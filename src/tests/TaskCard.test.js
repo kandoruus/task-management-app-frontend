61b5b36b558ba80fcca06190506c68a0c2632970
caleb <1d0nt3v3nl1ftbr0@gmail.com>
2023-11-22 10:35:14 -0500

    Completed unit tests for the TaskCard component

--- src/tests/TaskCard.test.js
+++ src/tests/TaskCard.test.js
@@ -4,10 +4,10 @@ import {
   initialTaskCtrlState,
   taskCtrlSlice,
   deleteTask,
+  saveOneTask,
 } from 'app/taskCtrlSlice';
 import { initialTaskEditorState, taskEditorSlice } from 'app/taskEditorSlice';
 import { getMockTasklist, renderWithProviders } from 'tests/testUtils';
-import { TaskEditor } from 'component/TaskListManagement/TaskEditor';
 import { setupStore } from 'app/store';
 import { TaskCard } from 'component/TaskListManagement/TaskCard';
 
@@ -30,21 +30,76 @@ describe('TaskCard', () => {
     expect(
       buttons.find((button) => button.name === 'task-delete-0')
     ).not.toBeUndefined();
-  }); /*
+  });
   it("displays the task's name as the task-button's text", () => {
-    renderWithProviders(<TaskCard idx={0} />);
-    expect(true).toBeFalsy;
-  }); /*
+    const mockedList = getMockTasklist(1);
+    const expectedText = mockedList[0].data.name;
+    renderWithProviders(<TaskCard idx={0} />, {
+      preloadedState: {
+        taskCtrl: { ...initialTaskCtrlState, tasklist: mockedList },
+        taskEditor: initialTaskEditorState,
+      },
+    });
+    expect(screen.getByText(expectedText)).toBeInTheDocument();
+  });
   it('dispatches the loadTaskData and openEditor actions when the task-button is clicked', () => {
-    renderWithProviders(<TaskCard idx={0} />);
-    expect(true).toBeFalsy;
-  });/*
-  it('dispatches the deleteTask action when the task-delete button is clicked', () => {
-    renderWithProviders(<TaskCard idx={0} />);
-    expect(true).toBeFalsy;
-  });/*
+    const mockedList = getMockTasklist(1);
+    const expectedData = mockedList[0].data;
+    const store = {
+      ...setupStore({
+        taskCtrl: { ...initialTaskCtrlState, tasklist: mockedList },
+        taskEditor: initialTaskEditorState,
+      }),
+      dispatch: jest.fn(),
+    };
+    renderWithProviders(<TaskCard idx={0} />, { store });
+    const taskButton = screen
+      .getAllByRole('button')
+      .find((button) => button.name === 'task-button-0');
+    fireEvent.click(taskButton);
+    expect(store.dispatch).toHaveBeenCalledWith(
+      taskCtrlSlice.actions.openEditor()
+    );
+    expect(store.dispatch).toHaveBeenCalledWith(
+      taskEditorSlice.actions.loadTaskData({ data: expectedData, indx: 0 })
+    );
+  });
   it('dispatches the saveOneTask action when the task-save button is clicked', () => {
-    renderWithProviders(<TaskCard idx={0} />);
-    expect(true).toBeFalsy;
-  });*/
+    const mockedList = getMockTasklist(1);
+    const expectedTask = mockedList[0];
+    const store = {
+      ...setupStore({
+        taskCtrl: { ...initialTaskCtrlState, tasklist: mockedList },
+        taskEditor: initialTaskEditorState,
+      }),
+      dispatch: jest.fn(),
+    };
+    renderWithProviders(<TaskCard idx={0} />, { store });
+    const taskSave = screen
+      .getAllByRole('button')
+      .find((button) => button.name === 'task-save-0');
+    fireEvent.click(taskSave);
+    expect(store.dispatch.mock.calls[0][0].toString()).toEqual(
+      saveOneTask(expectedTask).toString()
+    );
+  });
+  it('dispatches the deleteTask action when the task-delete button is clicked', () => {
+    const mockedList = getMockTasklist(1);
+    const expectedId = mockedList[0];
+    const store = {
+      ...setupStore({
+        taskCtrl: { ...initialTaskCtrlState, tasklist: mockedList },
+        taskEditor: initialTaskEditorState,
+      }),
+      dispatch: jest.fn(),
+    };
+    renderWithProviders(<TaskCard idx={0} />, { store });
+    const taskDelete = screen
+      .getAllByRole('button')
+      .find((button) => button.name === 'task-delete-0');
+    fireEvent.click(taskDelete);
+    expect(store.dispatch.mock.calls[0][0].toString()).toEqual(
+      deleteTask({ index: 0, taskId: expectedId }).toString()
+    );
+  });
 });
