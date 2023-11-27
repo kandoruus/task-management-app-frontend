965d8ac2d5d5f178478fded37c6e97ede114829d
caleb <1d0nt3v3nl1ftbr0@gmail.com>
2023-11-27 12:45:34 -0500

    Created useModal hook

--- src/app/hooks.ts
+++ src/app/hooks.ts
@@ -1,5 +1,20 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import type { RootState, AppDispatch } from './store';
+import { useState } from 'react';
+
+//useModal hook found here: https://upmostly.com/tutorials/modal-components-react-custom-hooks
+export const useModal = () => {
+  const [isShowing, setIsShowing] = useState(false);
+
+  function toggle() {
+    setIsShowing(!isShowing);
+  }
+
+  return {
+    isShowing,
+    toggle,
+  };
+};
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 type DispatchFunc = () => AppDispatch;
