df2ac1006612c64df5093b2b63ed90f350d69a34
caleb <1d0nt3v3nl1ftbr0@gmail.com>
2023-11-28 09:48:15 -0500

    cleaned TasklistViewer.test using BeforeEach

--- src/tests/TasklistViewer.test.js
+++ src/tests/TasklistViewer.test.js
@@ -5,8 +5,10 @@ import { renderWithProviders, getMockTasklist } from 'tests/testUtils';
 import { initialTaskCtrlState } from 'app/taskCtrlSlice';
 import { initialTaskEditorState } from 'app/taskEditorSlice';
 import { TASKS_PER_PAGE } from 'helper/constants';
+import { setupStore } from 'app/store';
 
 describe('TasklistViewer', () => {
+  const mocklist = getMockTasklist(101);
   it('renders TasklistViewer with no TaskCards', () => {
     renderWithProviders(<TasklistViewer />);
     expect(screen.getByTestId('tasklist-viewer')).toBeInTheDocument();
@@ -23,7 +25,7 @@ describe('TasklistViewer', () => {
       preloadedState: {
         taskCtrl: {
           ...initialTaskCtrlState,
-          tasklist: getMockTasklist(mockTasklistLength),
+          tasklist: mocklist.slice(0, mockTasklistLength),
         },
         taskEditor: initialTaskEditorState,
       },
@@ -39,7 +41,7 @@ describe('TasklistViewer', () => {
       preloadedState: {
         taskCtrl: {
           ...initialTaskCtrlState,
-          tasklist: getMockTasklist(mockTasklistLength),
+          tasklist: mocklist.slice(0, mockTasklistLength),
         },
         taskEditor: initialTaskEditorState,
       },
@@ -51,46 +53,57 @@ describe('TasklistViewer', () => {
     expect(screen.getByText('1 of 2')).toBeInTheDocument();
   });
   describe('with a large tasklist', () => {
-    beforeEach(() => {
-      const mockTasklistLength = 101;
-      renderWithProviders(<TasklistViewer />, {
-        preloadedState: {
-          taskCtrl: {
-            ...initialTaskCtrlState,
-            tasklist: getMockTasklist(mockTasklistLength),
-            page: 3,
-          },
-          taskEditor: initialTaskEditorState,
+    const store = {
+      ...setupStore({
+        taskCtrl: {
+          ...initialTaskCtrlState,
+          tasklist: mocklist,
+          page: 3,
         },
-      });
+        taskEditor: initialTaskEditorState,
+      }),
+    };
+    beforeEach(() => {
+      renderWithProviders(<TasklistViewer />, { store });
     });
-    it('displays exactly 25 TaskCards, the correct current page, and the correct number of pages', () => {
+    it('displays exactly 25 TaskCards, the correct current page, and the correct number of pages', async () => {
       expect(
         screen.queryByText('mock name ' + 2 * TASKS_PER_PAGE - 1)
       ).toBeNull();
-      for (let i = 2 * TASKS_PER_PAGE; i < 3 * TASKS_PER_PAGE; i++) {
-        expect(screen.getByText('mock name ' + i)).toBeInTheDocument();
-      }
+      const cards = await screen.findAllByText(/mock name \d+/);
+      expect(cards).toHaveLength(25);
+      expect(cards[0].textContent).toEqual('mock name ' + 2 * TASKS_PER_PAGE);
+      expect(cards[24].textContent).toEqual(
+        'mock name ' + (3 * TASKS_PER_PAGE - 1)
+      );
       expect(screen.queryByText('mock name ' + 3 * TASKS_PER_PAGE)).toBeNull();
       expect(screen.getByText('3 of 5')).toBeInTheDocument();
     });
-    it('navigates to the next page when the > button is clicked', () => {
+    it('navigates to the next page when the > button is clicked', async () => {
       fireEvent.click(screen.getByText('>'));
       expect(
         screen.queryByText('mock name ' + 3 * TASKS_PER_PAGE - 1)
       ).toBeNull();
-      for (let i = 3 * TASKS_PER_PAGE; i < 4 * TASKS_PER_PAGE; i++) {
-        expect(screen.getByText('mock name ' + i)).toBeInTheDocument();
-      }
-      expect(screen.queryByText('mock name ' + 2 * TASKS_PER_PAGE)).toBeNull();
+      const cards = await screen.findAllByText(/mock name \d+/);
+      expect(cards).toHaveLength(25);
+      expect(cards[0].textContent).toEqual('mock name ' + 3 * TASKS_PER_PAGE);
+      expect(cards[24].textContent).toEqual(
+        'mock name ' + (4 * TASKS_PER_PAGE - 1)
+      );
+      expect(screen.queryByText('mock name ' + 4 * TASKS_PER_PAGE)).toBeNull();
       expect(screen.getByText('4 of 5')).toBeInTheDocument();
     });
-    it('navigates to the previous page when the < button is clicked', () => {
+    it('navigates to the previous page when the < button is clicked', async () => {
+      fireEvent.click(screen.getByText('<'));
+      expect(await screen.findByText('3 of 5')).toBeInTheDocument();
       fireEvent.click(screen.getByText('<'));
       expect(screen.queryByText('mock name ' + TASKS_PER_PAGE - 1)).toBeNull();
-      for (let i = TASKS_PER_PAGE; i < 2 * TASKS_PER_PAGE; i++) {
-        expect(screen.getByText('mock name ' + i)).toBeInTheDocument();
-      }
+      const cards = await screen.findAllByText(/mock name \d+/);
+      expect(cards).toHaveLength(25);
+      expect(cards[0].textContent).toEqual('mock name ' + TASKS_PER_PAGE);
+      expect(cards[24].textContent).toEqual(
+        'mock name ' + (2 * TASKS_PER_PAGE - 1)
+      );
       expect(screen.queryByText('mock name ' + 2 * TASKS_PER_PAGE)).toBeNull();
       expect(screen.getByText('2 of 5')).toBeInTheDocument();
     });
@@ -101,30 +114,36 @@ describe('TasklistViewer', () => {
       expect(screen.queryByText('mock name 101')).toBeNull();
       expect(screen.getByText('5 of 5')).toBeInTheDocument();
     });
-    it('navigates to the first page when the First button is clicked', () => {
-      fireEvent.click(screen.getByText('First'));
-      for (let i = 0; i < TASKS_PER_PAGE; i++) {
-        expect(screen.getByText('mock name ' + i)).toBeInTheDocument();
-      }
-      expect(screen.queryByText('mock name ' + TASKS_PER_PAGE)).toBeNull();
-      expect(screen.getByText('1 of 5')).toBeInTheDocument();
-    });
-    it('does not change the page when the < is clicked and the current page is the first page', () => {
-      fireEvent.click(screen.getByText('First'));
-      fireEvent.click(screen.getByText('<'));
-      for (let i = 0; i < TASKS_PER_PAGE; i++) {
-        expect(screen.getByText('mock name ' + i)).toBeInTheDocument();
-      }
-      expect(screen.queryByText('mock name ' + TASKS_PER_PAGE)).toBeNull();
-      expect(screen.getByText('1 of 5')).toBeInTheDocument();
-    });
-    it('does not change the page when the > is clicked and the current page is the last page', () => {
-      fireEvent.click(screen.getByText('Last'));
+    it('does not change the page when the > is clicked and the current page is the last page', async () => {
+      expect(screen.getByText('5 of 5')).toBeInTheDocument();
       fireEvent.click(screen.getByText('>'));
       expect(screen.queryByText('mock name 99')).toBeNull();
       expect(screen.getByText('mock name 100')).toBeInTheDocument();
       expect(screen.queryByText('mock name 101')).toBeNull();
       expect(screen.getByText('5 of 5')).toBeInTheDocument();
+    });
+    it('navigates to the first page when the First button is clicked', async () => {
+      fireEvent.click(screen.getByText('First'));
+      const cards = await screen.findAllByText(/mock name \d+/);
+      expect(cards).toHaveLength(25);
+      expect(cards[0].textContent).toEqual('mock name 0');
+      expect(cards[24].textContent).toEqual(
+        'mock name ' + (TASKS_PER_PAGE - 1)
+      );
+      expect(screen.queryByText('mock name ' + TASKS_PER_PAGE)).toBeNull();
+      expect(screen.getByText('1 of 5')).toBeInTheDocument();
+    });
+    it('does not change the page when the < is clicked and the current page is the first page', async () => {
+      expect(screen.getByText('1 of 5')).toBeInTheDocument();
+      fireEvent.click(screen.getByText('<'));
+      const cards = await screen.findAllByText(/mock name \d+/);
+      expect(cards).toHaveLength(25);
+      expect(cards[0].textContent).toEqual('mock name 0');
+      expect(cards[24].textContent).toEqual(
+        'mock name ' + (TASKS_PER_PAGE - 1)
+      );
+      expect(screen.queryByText('mock name ' + TASKS_PER_PAGE)).toBeNull();
+      expect(screen.getByText('1 of 5')).toBeInTheDocument();
     });
   });
 });
