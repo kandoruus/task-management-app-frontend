a68e0de5fbad2bfe39c62d1802869155e79b21a1
caleb <1d0nt3v3nl1ftbr0@gmail.com>
2023-11-30 14:46:12 -0500

    set up the home page and styled

--- src/component/master/MasterContainer/home/HomeMasterPane/HomeMasterPane.tsx
+++ src/component/master/MasterContainer/home/HomeMasterPane/HomeMasterPane.tsx
@@ -1,11 +1,61 @@
 import React from 'react';
-import { Box } from '@mui/material';
+import { Box, Grid, Button, Typography } from '@mui/material';
 import './HomeMasterPane.css';
+import { appCtrlSlice } from 'app/appCtrlSlice';
+import { useAppDispatch } from 'app/hooks';
+import { AppDispatch } from 'app/store';
 
 export const HomeMasterPane: React.FC = () => {
+  const dispatch: AppDispatch = useAppDispatch();
+  const handleTasksClick = () => {
+    dispatch(appCtrlSlice.actions.focusTasks());
+  };
+  const handleTimesheetClick = () => {
+    dispatch(appCtrlSlice.actions.focusTimesheet());
+  };
+  const handleAccountClick = () => {
+    dispatch(appCtrlSlice.actions.focusAccount());
+  };
+  const handleSettingsClick = () => {
+    dispatch(appCtrlSlice.actions.focusSettings());
+  };
+  const handleAdminClick = () => {
+    dispatch(appCtrlSlice.actions.focusAdmin());
+  };
   return (
-    <Box className="home-master-pane" data-testid="home-master-pane">
-      HOME
+    <Box className="master-pane" data-testid="home-master-pane">
+      <Box className="home-page">
+        <Typography color="primary" variant="h4">
+          Welcome to the Task Management App!
+        </Typography>
+        <Grid container spacing={0}>
+          <Grid item>
+            <Button variant="contained" onClick={handleTasksClick}>
+              Tasks
+            </Button>
+          </Grid>
+          <Grid item>
+            <Button variant="contained" onClick={handleTimesheetClick}>
+              Timesheet
+            </Button>
+          </Grid>
+          <Grid item>
+            <Button variant="contained" onClick={handleAccountClick}>
+              Account
+            </Button>
+          </Grid>
+          <Grid item>
+            <Button variant="contained" onClick={handleSettingsClick}>
+              Settings
+            </Button>
+          </Grid>
+          <Grid item>
+            <Button variant="contained" onClick={handleAdminClick}>
+              Administration
+            </Button>
+          </Grid>
+        </Grid>
+      </Box>
     </Box>
   );
 };
