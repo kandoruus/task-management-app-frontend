9b39b1e4a1c9ca35285700b69a5646f61a740bf9
caleb <1d0nt3v3nl1ftbr0@gmail.com>
2023-12-11 13:46:33 -0500

    wrote tests for WelcomePage with component passing

--- src/component/welcome/WelcomePage/WelcomePage.test.js
+++ src/component/welcome/WelcomePage/WelcomePage.test.js
@@ -1,25 +1,32 @@
 import * as React from 'react';
-//import { screen, fireEvent } from '@testing-library/react';
+import { screen, fireEvent } from '@testing-library/react';
 import { renderWithProviders } from 'helper/testUtils';
 import { WelcomePage } from './WelcomePage';
-import { setupStore } from 'app/store';
+import { PAGES } from 'helper/constants';
+
 const mockedNavigator = jest.fn();
 jest.mock('react-router-dom', () => ({
   ...jest.requireActual('react-router-dom'),
   useNavigate: () => mockedNavigator,
 }));
+
 describe('WelcomePage', () => {
-  const store = {
-    ...setupStore(),
-    dispatch: jest.fn(),
-  };
   beforeEach(() => {
-    renderWithProviders(<WelcomePage />, { store });
+    renderWithProviders(<WelcomePage />);
   });
   afterEach(() => {
-    store.dispatch.mockClear();
+    mockedNavigator.mockClear();
   });
-  it('TODO:', () => {
-    expect(true).toBe(false);
+  it('renders the welcome page', () => {
+    expect(screen.getByText('Login')).toBeInTheDocument();
+    expect(screen.getByText('Sign Up')).toBeInTheDocument();
+  });
+  it('navigates to the login page when the login button is clicked', () => {
+    fireEvent.click(screen.getByText('Login'));
+    expect(mockedNavigator).toHaveBeenCalledWith(PAGES.LOGIN);
+  });
+  it('navigates to the signup page when the sign up button is clicked', () => {
+    fireEvent.click(screen.getByText('Sign Up'));
+    expect(mockedNavigator).toHaveBeenCalledWith(PAGES.SIGNUP);
   });
 });
