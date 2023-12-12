fd2f9ee90409a446c1f9f12565b43a90a5bb66b9
caleb <1d0nt3v3nl1ftbr0@gmail.com>
2023-12-11 22:33:20 -0500

    simpified the LoginPage component using generic component CredentialsInput. Wrote unit test for component which passed

--- src/component/welcome/LoginPage/LoginPage.test.js
+++ src/component/welcome/LoginPage/LoginPage.test.js
@@ -1,25 +1,146 @@
 import * as React from 'react';
-//import { screen, fireEvent } from '@testing-library/react';
+import { screen, fireEvent, waitFor } from '@testing-library/react';
 import { renderWithProviders } from 'helper/testUtils';
 import { LoginPage } from './LoginPage';
 import { setupStore } from 'app/store';
+import { COOKIES, ERR_MSG, PAGES } from 'helper/constants';
+import { login } from 'app/slices/appCtrlSlice';
+
+const mockedCookie = jest.fn();
+const mockedSetCookie = jest.fn();
 const mockedNavigator = jest.fn();
+const sendAlert = jest.fn();
 jest.mock('react-router-dom', () => ({
   ...jest.requireActual('react-router-dom'),
   useNavigate: () => mockedNavigator,
 }));
+jest.mock('react-cookie', () => ({
+  ...jest.requireActual('react-cookie'),
+  useCookies: () => [mockedCookie, mockedSetCookie],
+}));
+const store = {
+  ...setupStore(),
+  dispatch: jest.fn(),
+};
+const validUsername = 'validUsername';
+const validPassword = 'validPassword';
+
 describe('LoginPage', () => {
-  const store = {
-    ...setupStore(),
-    dispatch: jest.fn(),
-  };
   beforeEach(() => {
-    renderWithProviders(<LoginPage />, { store });
+    renderWithProviders(<LoginPage sendAlert={sendAlert} />, { store });
   });
   afterEach(() => {
     store.dispatch.mockClear();
+    sendAlert.mockClear();
+    mockedNavigator.mockClear();
+    mockedSetCookie.mockClear();
   });
-  it('TODO:', () => {
-    expect(true).toBe(false);
+  it('renders the login page', () => {
+    expect(screen.getByLabelText('Username')).toBeInTheDocument();
+    expect(screen.getByLabelText('Password')).toBeInTheDocument();
+    expect(screen.getByText("Don't have an account?")).toBeInTheDocument();
+    expect(screen.getByText('Sign Up')).toBeInTheDocument();
+    expect(screen.getByText('Login')).toBeInTheDocument();
+  });
+  it('navigates to the signup page when the signup button is clicked', () => {
+    fireEvent.click(screen.getByText('Sign Up'));
+    expect(mockedNavigator).toHaveBeenCalledWith(PAGES.SIGNUP);
+  });
+  it('sends the ERR_MSG.INPUT_IS_BLANK message when the login button is clicked and the username field is blank', () => {
+    fireEvent.change(screen.getByLabelText('Password'), {
+      target: { value: validPassword },
+    });
+    fireEvent.click(screen.getByText('Login'));
+    expect(sendAlert).toHaveBeenCalledWith(ERR_MSG.INPUT_IS_BLANK);
+    expect(store.dispatch).not.toHaveBeenCalled();
+  });
+  it('sends the ERR_MSG.INPUT_IS_BLANK message when the login button is clicked and the password field is blank', () => {
+    fireEvent.change(screen.getByLabelText('Username'), {
+      target: { value: validUsername },
+    });
+    fireEvent.click(screen.getByText('Login'));
+    expect(sendAlert).toHaveBeenCalledWith(ERR_MSG.INPUT_IS_BLANK);
+    expect(store.dispatch).not.toHaveBeenCalled();
+  });
+  describe('when the password and username are valid and the login button is clicked', () => {
+    beforeEach(() => {
+      fireEvent.change(screen.getByLabelText('Username'), {
+        target: { value: validUsername },
+      });
+      fireEvent.change(screen.getByLabelText('Password'), {
+        target: { value: validPassword },
+      });
+    });
+    it('dispatches the login action and sends the ERR_MSG.LOGIN_FAILED message if the response is undefined', async () => {
+      store.dispatch.mockReturnValueOnce({
+        payload: undefined,
+      });
+      fireEvent.click(screen.getByText('Login'));
+      expect(store.dispatch.mock.calls[0][0].toString()).toEqual(
+        login({ username: validUsername, password: validPassword }).toString()
+      );
+      await waitFor(() =>
+        expect(sendAlert).toHaveBeenCalledWith(ERR_MSG.LOGIN_FAILED)
+      );
+    });
+    it('dispatches the login action and sends the message if the response status is not "success"', async () => {
+      const mockedErrorMessage = 'mockedErrorMessage';
+      store.dispatch.mockReturnValueOnce({
+        payload: {
+          data: {
+            message: mockedErrorMessage,
+            username: '',
+            sessionCode: '',
+          },
+          status: 'error',
+        },
+      });
+      fireEvent.click(screen.getByText('Login'));
+
+      expect(store.dispatch.mock.calls[0][0].toString()).toEqual(
+        login({ username: validUsername, password: validPassword }).toString()
+      );
+      await waitFor(() =>
+        expect(sendAlert).toHaveBeenCalledWith(mockedErrorMessage)
+      );
+    });
+    it('dispatches the login action and sets the login and session data cookies when the response status is success', async () => {
+      const mockedSessionCode = 'mockedSessionCode';
+      store.dispatch.mockReturnValueOnce({
+        payload: {
+          data: {
+            message: '',
+            username: validUsername,
+            sessionCode: mockedSessionCode,
+          },
+          status: 'success',
+        },
+      });
+      fireEvent.click(screen.getByText('Login'));
+      expect(store.dispatch.mock.calls[0][0].toString()).toEqual(
+        login({ username: validUsername, password: validPassword }).toString()
+      );
+      await waitFor(() =>
+        expect(mockedSetCookie).toHaveBeenCalledWith(
+          COOKIES.LOGIN,
+          COOKIES.LOGIN,
+          { path: '/' }
+        )
+      );
+      await waitFor(() =>
+        expect(mockedSetCookie).toHaveBeenCalledWith(
+          COOKIES.USERNAME,
+          validUsername,
+          { path: '/' }
+        )
+      );
+      await waitFor(() =>
+        expect(mockedSetCookie).toHaveBeenCalledWith(
+          COOKIES.SESSIONCODE,
+          mockedSessionCode,
+          { path: '/' }
+        )
+      );
+    });
   });
 });
--- src/component/welcome/LoginPage/LoginPage.tsx
+++ src/component/welcome/LoginPage/LoginPage.tsx
@@ -1,105 +1,66 @@
 import React, { useState } from 'react';
 import './LoginPage.css';
-import {
-  Box,
-  Button,
-  FormControl,
-  IconButton,
-  InputAdornment,
-  InputLabel,
-  OutlinedInput,
-  Typography,
-} from '@mui/material';
-import { VisibilityOff, Visibility } from '@mui/icons-material';
-import axios from 'axios';
-import { USER_API, AXIOS_HEADERS, COOKIES, PAGES } from 'helper/constants';
+import { Box, Button, Typography } from '@mui/material';
+import { COOKIES, PAGES, ERR_MSG } from 'helper/constants';
 import { useNavigate } from 'react-router-dom';
 import { useCookies } from 'react-cookie';
+import { CredentialsInput } from 'component/helper-components/CredentialsInput/CredentialsInput';
+import { areBlankInputs } from 'helper/functions';
+import { useAppDispatch } from 'app/hooks';
+import { login } from 'app/slices/appCtrlSlice';
 
 type Props = {
   sendAlert: (message: string) => void;
 };
 
 export const LoginPage: React.FC<Props> = (props) => {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [cookies, setCookie] = useCookies([
+  const setCookie = useCookies([
     COOKIES.USERNAME,
     COOKIES.SESSIONCODE,
     COOKIES.LOGIN,
-  ]);
+  ])[1];
   const navigate = useNavigate();
+  const dispatch = useAppDispatch();
   const { sendAlert } = props;
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const [showPassword, setShowPassword] = useState(false);
-  const handleMouseDownPassword = (
-    event: React.MouseEvent<HTMLButtonElement>
-  ) => {
-    event.preventDefault();
-  };
-  const handleClickShowPassword = () => setShowPassword((show) => !show);
   const handleLogin = async () => {
-    try {
-      const resData = (
-        await axios.post(
-          USER_API.LOGIN,
-          {
-            username,
-            password,
-          },
-          AXIOS_HEADERS
-        )
-      ).data;
-      setCookie(COOKIES.LOGIN, COOKIES.LOGIN, { path: '/' });
-      setCookie(COOKIES.USERNAME, resData.username, { path: '/' });
-      setCookie(COOKIES.SESSIONCODE, resData.sessionCode, { path: '/' });
-    } catch (e) {
-      if (axios.isAxiosError(e) && e.response) {
-        sendAlert(e.response.data.message);
+    if (areBlankInputs([password, username])) {
+      sendAlert(ERR_MSG.INPUT_IS_BLANK);
+    } else {
+      const res = (await dispatch(login({ username, password }))).payload as
+        | {
+            data: { message: string; username: string; sessionCode: string };
+            status: string;
+          }
+        | undefined;
+      if (res === undefined) {
+        sendAlert(ERR_MSG.LOGIN_FAILED);
+      } else if (res.status === 'success') {
+        setCookie(COOKIES.LOGIN, COOKIES.LOGIN, { path: '/' });
+        setCookie(COOKIES.USERNAME, res.data.username, { path: '/' });
+        setCookie(COOKIES.SESSIONCODE, res.data.sessionCode, { path: '/' });
       } else {
-        console.error(e);
+        sendAlert(res.data.message);
       }
     }
   };
 
   return (
     <Box className="login-inner-wrapper" data-testid="login-page">
-      <FormControl sx={{ m: 1, width: '35ch' }} variant="outlined">
-        <InputLabel htmlFor="outlined-username">Username</InputLabel>
-        <OutlinedInput
-          id="outlined-username"
-          type="text"
-          label="Username"
-          value={username}
-          onChange={(e) => {
-            setUsername(e.target.value);
-          }}
-        />
-      </FormControl>
-      <FormControl sx={{ m: 1, width: '35ch' }} variant="outlined">
-        <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
-        <OutlinedInput
-          id="outlined-adornment-password"
-          type={showPassword ? 'text' : 'password'}
-          endAdornment={
-            <InputAdornment position="end">
-              <IconButton
-                aria-label="toggle password visibility"
-                onClick={handleClickShowPassword}
-                onMouseDown={handleMouseDownPassword}
-                edge="end"
-              >
-                {showPassword ? <VisibilityOff /> : <Visibility />}
-              </IconButton>
-            </InputAdornment>
-          }
-          label="Password"
-          value={password}
-          onChange={(e) => {
-            setPassword(e.target.value);
-          }}
-        />
-      </FormControl>
+      <CredentialsInput
+        input={username}
+        setInput={setUsername}
+        label="Username"
+        id="username-input"
+      />
+      <CredentialsInput
+        input={password}
+        setInput={setPassword}
+        label="Password"
+        id="password-input"
+        isPassword
+      />
       <Box
         sx={{
           display: 'flex',
