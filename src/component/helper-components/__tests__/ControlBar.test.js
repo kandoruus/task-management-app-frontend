119ab39ec89e658e25700a9753134313345d1b36
caleb <1d0nt3v3nl1ftbr0@gmail.com>
2023-12-18 11:17:04 -0500

    created test file for ControlBar

---
+++ src/component/helper-components/__tests__/ControlBar.test.js
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import { fireEvent, screen, render } from '@testing-library/react';
+import { ControlBar } from '../ControlBar';
+
+describe('ControlBar', () => {
+  it('TODO: write tests', () => {
+    expect(true).toBe(false);
+  });
+});
