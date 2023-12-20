38c7fa372bd360e8101287e06a302bd62c84e1b4
caleb <1d0nt3v3nl1ftbr0@gmail.com>
2023-12-20 11:05:39 -0500

    added hook to select tasks based on a list of task ids

--- src/app/hooks.ts
+++ src/app/hooks.ts
@@ -1,6 +1,7 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import type { RootState, AppDispatch } from './store';
 import { useState } from 'react';
+import { selectTaskCtrl } from 'app/slices/taskCtrlSlice';
 
 //useModal hook base on this article: https://upmostly.com/tutorials/modal-components-react-custom-hooks
 export const useModal = (): [boolean, () => void] => {
@@ -11,6 +12,15 @@ export const useModal = (): [boolean, () => void] => {
   }
 
   return [isOpen, toggle];
+};
+
+//get the tasklist filtered by a list of ids
+export const useTasklistByIds = (taskIds: string[]) => {
+  return useAppSelector((state) =>
+    selectTaskCtrl(state).tasklist.filter((task) => {
+      return taskIds.includes(task._id);
+    })
+  );
 };
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
